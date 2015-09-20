da33fba99fa6a6496f49f82defa9e345edc7c651
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-09-20 11:14:49 -0400

    added serializers for API

--- forecast-admin/forecast/opportunities/views.py
+++ forecast-admin/forecast/opportunities/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import Award
+from .models import Award, Office
 from rest_framework import viewsets
-from .serializers import AwardSerializer
+from .serializers import AwardSerializer, OfficeSerializer
 
 class AwardViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -9,3 +9,10 @@ class AwardViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Award.objects.all()
     serializer_class = AwardSerializer
+
+class OfficeViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Office.objects.all()
+    serializer_class = OfficeSerializer
