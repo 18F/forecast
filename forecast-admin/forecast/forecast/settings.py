e976d206b74db334352771d4808c2cf6e9cc0a63
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-11-21 13:40:32 -0500

    codeclimate and some more cleaning

--- forecast-admin/forecast/forecast/settings.py
+++ forecast-admin/forecast/forecast/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY") or '!=uy%w6m1jgg9$!+z!+f!z)_uzo8$8+0m%&ye8e!-*by*w40g7'
+SECRET_KEY = os.environ.get("SECRET_KEY") or '!=uy%w6m1jgg9$!+z!+f!z)\
+    _uzo8$8+0m%&ye8e!-*by*w40g7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,11 +82,10 @@ WSGI_APPLICATION = 'forecast.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-import dj_database_url
 DATABASES = {}
 
 if not os.environ.get("DATABASE_URL"):
-   DATABASES = {
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -93,7 +94,6 @@ if not os.environ.get("DATABASE_URL"):
 else:
     DATABASE_URL = os.environ.get("DATABASE_URL")
     DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
-
 
 
 # Internationalization
