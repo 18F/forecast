ee6a48cfc8d9e71ac323ca84121f4bc929d79685
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-11-21 14:32:24 -0500

    Travis and coverage
    Per https://github.com/django-nose/django-nose/issues/180, I updated
    the settings.py file to not import models before testing.
    
    Also, updated the API for cloud.gov.

--- forecast-admin/forecast/manage.py
+++ forecast-admin/forecast/manage.py
@@ -7,4 +7,19 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    is_testing = 'test' in sys.argv
+
+    if is_testing:
+        import coverage
+        cov = coverage.coverage(source=['opportunities'],
+                                omit=['opportunities/migrations/**',
+                                      '*/tests.py'])
+        cov.erase()
+        cov.start()
+
     execute_from_command_line(sys.argv)
+
+    if is_testing:
+        cov.stop()
+        cov.save()
+        cov.report()
