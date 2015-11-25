3b0dd0c30b13093e19400865b90874099a265ace
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-11-25 14:59:56 -0500

    functioning tests and template

---
+++ forecast-admin/forecast/opportunities/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
+from opportunities.views import home
+
+
+class ViewsTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_home_view(self):
+        request = self.factory.get(reverse('home'))
+        response = home(request)
+        self.assertEqual(response.status_code, 200)
