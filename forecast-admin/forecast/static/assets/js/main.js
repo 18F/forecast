54c8b27afe366f7f3fb0319d7ba3f17846dc88e1
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-11-27 08:06:59 -0500

    JSON loaded on home

--- forecast-admin/forecast/static/assets/js/main.js
+++ forecast-admin/forecast/static/assets/js/main.js
@@ -1,7 +1,8 @@
 
 // Get the opportunities from the API
-/*
 $.getJSON('/api/opportunities/?format=json', function (d){
+  d.map(function (o){
+    return $("#results").append("<li>" + o.description + "</li>")
+  })
   console.log(d);
 })
-*/
