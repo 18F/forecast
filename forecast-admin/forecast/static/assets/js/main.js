45f988f167aec81c6ba406619895750e69a6ec8e
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-11-27 12:43:07 -0500

    ready

--- forecast-admin/forecast/static/assets/js/main.js
+++ forecast-admin/forecast/static/assets/js/main.js
@@ -2,19 +2,18 @@
 // Get the opportunities from the API
 $.getJSON('/api/opportunities/?format=json', function (d){
   $("#results").html('');
+  var i = 0;
   d.map(function (o){
-
-
-
-
-    return $("#results").append(createRow(o))
+    i++;
+    return $("#results").append(createRow(o, i))
   })
   console.log(d);
 })
 
 
-function createRow(o) {
-
-  var res = '<div class="opportunity-row"><div class="office">' + o.office + '</div><div class="description">' + o.description + '</div><div class="usa-grid"> <div class="usa-width-one-half"><p><span class="detail-label">Award Status: </span>' + o.award_status + '</p><p><span class="detail-label">Place of Performance: </span>' + o.place_of_performance_city + ", " + o.place_of_performance_state + '</p><p><span class="detail-label">NAICS Code: </span>' + o.naics + '</p></div><div class="usa-width-one-half"><p><span class="detail-label">Estimated Award Date: </span>FY ' + o.estimated_fiscal_year + '- Quarter' +  o.estimated_fiscal_year_quarter + '</p><p><span class="detail-label">Category: </span>' + o.socioeconomic + '</p> <p><a href="">View Details </a></p></div></div></div>'
+function createRow(o,i) {
+  var res = '<div class="opportunity-row';
+  res += (i % 2 === 1 ? '">' : ' opportunity-row-alternate">');
+  res += '<div class="office">' + o.office + '</div><div class="description">' + o.description + '</div><div class="usa-grid"> <div class="usa-width-one-half"><p><span class="detail-label">Award Status: </span>' + o.award_status + '</p><p><span class="detail-label">Place of Performance: </span>' + o.place_of_performance_city + ", " + o.place_of_performance_state + '</p><p><span class="detail-label">NAICS Code: </span>' + o.naics + '</p></div><div class="usa-width-one-half"><p><span class="detail-label">Estimated Award Date: </span>FY ' + o.estimated_fiscal_year + '- Quarter' +  o.estimated_fiscal_year_quarter + '</p><p><span class="detail-label">Category: </span>' + o.socioeconomic + '</p> <p><a href="">View Details </a></p></div></div></div>'
   return res;
 }
