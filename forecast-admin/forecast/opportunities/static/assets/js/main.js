c2e163cde1a4c47829051bd2c13286aa29576c15
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-12-04 17:01:32 -0800

    functioning list filter
    This commit adds a dependency, list.js (https://github.com/javve/list.js),
    and uses the depedency to allow for client-side filtering of the opportunities.
    
    It also adds the beginnings of the dropdown filters.

--- forecast-admin/forecast/opportunities/static/assets/js/main.js
+++ forecast-admin/forecast/opportunities/static/assets/js/main.js
@@ -1,20 +1,11 @@
 
-// Get the opportunities from the API
-$.getJSON('/api/opportunities/?format=json', function (d){
-  $("#results").html('');
-  d.map(function (o){
+// This is going to load the GSA data
+$.getJSON('/api/opportunities/?format=json')
+  .done(function (d) {
 
-
-
-
-    return $("#results").append(createRow(o))
-  })
-  console.log(d);
+    var options = {
+        item: 'opportunity-row',
+        valueNames: ['description','naics']
+    };
+    new List('results', options, d);
 })
-
-
-function createRow(o) {
-
-  var res = '<div class="opportunity-row"><div class="office">' + o.office + '</div><div class="description">' + o.description + '</div><div class="usa-grid"> <div class="usa-width-one-half"><p><span class="detail-label">Award Status: </span>' + o.award_status + '</p><p><span class="detail-label">Place of Performance: </span>' + o.place_of_performance_city + ", " + o.place_of_performance_state + '</p><p><span class="detail-label">NAICS Code: </span>' + o.naics + '</p></div><div class="usa-width-one-half"><p><span class="detail-label">Estimated Award Date: </span>FY ' + o.estimated_fiscal_year + '- Quarter' +  o.estimated_fiscal_year_quarter + '</p><p><span class="detail-label">Category: </span>' + o.socioeconomic + '</p> <p><a href="">View Details </a></p></div></div></div>'
-  return res;
-}
