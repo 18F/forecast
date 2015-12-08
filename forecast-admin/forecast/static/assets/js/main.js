45ae94bb71e15c7fd7fb8f16fa7e80b1b367c411
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-12-08 12:35:50 -0500

    Multiple changes: Other agencies, frontend++
    The majority of the changes here are visual improvements to the frontend and changes to the
    css. The UX is modified to allow for greater filtering.
    
    Also added base data from State and Ed for demo.

--- forecast-admin/forecast/static/assets/js/main.js
+++ forecast-admin/forecast/static/assets/js/main.js
@@ -1,15 +1,117 @@
 var fullResults;
-// This is going to load the GSA data
-$.getJSON('/api/opportunities/?format=json')
-  .then(function (d) {
-    $("#loading").remove();
-    var options = {
-        item: 'opportunity-row',
-        indexAsync: true,
-        valueNames: ['description','naics']
-    };
-    new List('results', options, d);
+var data;
 
-}).then(function () {
-  fullResults = $("#opportunities").clone()
-})
+_initializeFilters();
+_loadGSAdata()
+
+
+/**
+ * Load the GSA data
+ **/
+function _loadGSAdata() {
+  // This is going to load the GSA data
+  $.getJSON('/api/opportunities/?format=json')
+    .done(function (d) {
+      d["agency"] = "gsa";
+      _loadFilterOptions(d);
+
+      $("#loading").remove();
+      var options = {
+          item: 'opportunity-row',
+          valueNames: ['description','office','naics']
+      };
+      var listObj = new List('results', options, d);
+      data = d;
+      // Create Filter events
+      _createFilterEvents(['award_status','naics','place_of_performance_state'],
+        listObj);
+
+      _loadOtherAgencies(['state','ed'], listObj);
+
+  }).then(function () {
+    fullResults = $("#opportunities").clone()
+  })
+}
+
+/**
+ *Initializes the More/Fewer Filters
+ **/
+function _initializeFilters() {
+  // init
+  $("#award-amount-dropdown").parent().toggle();
+  $("#estimated_fiscal_year_quarter-dropdown").parent().toggle();
+  $("#more-filters").on('click', function(e){
+    $("#more-filters").text("Fewer Filters");
+    $("#award-amount-dropdown").parent().toggle();
+    $("#estimated_fiscal_year_quarter-dropdown").parent().toggle();
+  })
+}
+
+/**
+ *Initializes the More/Fewer Filters
+ **/
+
+function _loadFilterOptions(d) {
+  data = d;
+  _getOptions(d, 'award_status');
+  _getOptions(d, 'naics');
+  _getOptions(d, 'place_of_performance_state');
+}
+
+/**
+ * Load the Data into the Filters
+ **/
+function _getOptions(d, field) {
+  var opt = _.uniq(_.pluck(_.sortBy(d, field), field));
+  $.each(opt, function(key, value) {
+     $('#' + field + '-dropdown')
+         .append($("<option></option>")
+         .attr("value",value)
+         .text(value));
+  });
+}
+
+/**
+ * Initializes event for Filters
+ **/
+function _createFilterEvents(filters, listObj) {
+
+  _.each(filters, function(d) {
+
+    var dropdown = "#" + d + "-dropdown";
+    $(dropdown).change(function (){
+      listObj.filter(function(item) {
+        var val = $(dropdown).val();
+        if (val === "all") {
+          return true;
+        }
+        else {
+          return (item.values()[d] === val ? true : false);
+        }
+      })
+    })
+  })
+}
+
+
+/**
+ * Initializes event for Filters
+ **/
+function _loadOtherAgencies (agencies, listObj) {
+  _.each(agencies, function (agency){
+    $.getJSON('/static/data/fy16' + agency + '.json').done(function (d){
+      d['agency'] = agency;
+      listObj.add(d);
+    })
+  })
+  var dropdown = "#agency-dropdown";
+  $(dropdown).change(function (){
+    listObj.filter(function (item) {
+      var val = $(dropdown).val();
+      if (val === "gsa") {
+        return (item.values()["agency"] ? false : true);  
+      }
+      return (item.values()["agency"] === val ? true : false);
+    })
+  })
+}
