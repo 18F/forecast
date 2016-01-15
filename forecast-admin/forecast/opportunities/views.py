5106f351dd9b465f1b0d6d89eeeabb50f2bf0e71
stvnrlly <steven.reilly@gsa.gov>
2016-01-15 09:35:36 -0500

    add opportunity detail template

--- forecast-admin/forecast/opportunities/views.py
+++ forecast-admin/forecast/opportunities/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Opportunity, Office, OSBUAdvisor
 from rest_framework import viewsets
+from django.core import serializers
+import json
 from .serializers import (
     OpportunitySerializer, OfficeSerializer, OSBUAdvisorSerializer
 )
@@ -10,8 +12,13 @@ def home(request):
     return render(request, 'main.html')
 
 
-def details(request):
-    return render(request, 'detail.html')
+def details(request, id):
+    """
+    A page displaying details about a particular contracting opportunity
+    """
+    opportunity = serializers.serialize("json", Opportunity.objects.all().filter(id=id))
+    opportunity = json.loads(opportunity)
+    return render(request, 'detail.html', {'o': opportunity[0]["fields"]})
 
 
 class OpportunityViewSet(viewsets.ReadOnlyModelViewSet):
