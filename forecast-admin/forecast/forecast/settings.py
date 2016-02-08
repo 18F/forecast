88d54987c327a552c15b9a5460af4c10ed59e72a
stvnrlly <steven.reilly@gsa.gov>
2016-02-08 17:06:44 -0500

    switch to postgresql db

--- forecast-admin/forecast/forecast/settings.py
+++ forecast-admin/forecast/forecast/settings.py
@@ -85,8 +85,12 @@ DATABASES = {}
 if not os.environ.get("DATABASE_URL"):
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get("DATABASE_NAME"),
+            'USER': os.environ.get("DATABASE_USER"),
+            'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+            'HOST': os.environ.get("DATABASE_HOST"),
+            'PORT': os.environ.get("DATABASE_PORT"),
         }
     }
 else:
