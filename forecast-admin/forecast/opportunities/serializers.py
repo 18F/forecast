11ab459701aafc6127bbd9c43a9cd84fbf7a50b1
stvnrlly <steven.reilly@gsa.gov>
2016-08-01 12:14:43 -0400

    include region in opportunities

--- forecast-admin/forecast/opportunities/serializers.py
+++ forecast-admin/forecast/opportunities/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
 from .models import Opportunity, Office, OSBUAdvisor
 
+class OfficeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Office
+        fields = ('organization', 'region')
+        
 
 class OpportunitySerializer(serializers.ModelSerializer):
-    office = serializers.ReadOnlyField(source='office.organization')
+    # office = serializers.ReadOnlyField(source='office.organization')
+    office = OfficeSerializer()
 
     class Meta:
         model = Opportunity
         # exclude = ('owner',)
-
-
-class OfficeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Office
 
 
 class OSBUAdvisorSerializer(serializers.ModelSerializer):
